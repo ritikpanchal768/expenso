4f30028740e9588bd644da27e4e8e374a513fa23
Ritik Panchal <44132205+ritikpanchal768@users.noreply.github.com>
2025-01-01 20:17:57 +0530

    new dransaction display response with category

--- expenso/src/main/java/com/example/expenso/transaction/TransactionController.java
+++ expenso/src/main/java/com/example/expenso/transaction/TransactionController.java
@@ -5,14 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController                                                     //Add REST Support
 @CrossOrigin
 @RequestMapping("/api/v1/transaction")
 public class TransactionController {
     private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
-    @GetMapping("/view/{referenceNumber}")
-    public CommonResponse addExpense(@PathVariable String referenceNumber)throws Exception {
+    @GetMapping("/viewByReferenceNumber/{referenceNumber}")
+    public CommonResponse viewByrefernceNumber(@PathVariable String referenceNumber)throws Exception {
         logger.info("************* Inside Get Transaction By ReferenceNo Controller ****************");
         CommonResponse commonResponse = new CommonResponse();
         try {
@@ -21,6 +23,20 @@ public class TransactionController {
             commonResponse.setCode("200");
             commonResponse.setResponseMessage("Successfully Fetched");
             commonResponse.setResponseObject(transactionInfo);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+        return commonResponse;
+    }
+    @GetMapping("/viewByMobileNumber/{mobileNumber}")
+    public CommonResponse viewByMobileNumber(@PathVariable String mobileNumber)throws Exception {
+        logger.info("************* Inside Get Transactions By MobileNumber Controller ****************");
+        CommonResponse commonResponse = new CommonResponse();
+        try {
+            List<TransactionDisplay> transactionDisplays = new TransactionDataAccess().getByMobileNumber(mobileNumber);
+            commonResponse.setCode("200");
+            commonResponse.setResponseMessage("Successfully Fetched");
+            commonResponse.setResponseObject(transactionDisplays);
         } catch (Exception e) {
             throw new Exception(e);
         }
